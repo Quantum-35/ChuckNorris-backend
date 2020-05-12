15ed0dfa10f84dbb30f99141cc3750adbb4007d4
Quantum-35 <quantumcomputing35@icloud.com>
2020-05-12 19:44:31 +0300

    Add get category jokes and categories

--- src/resolvers/query/index.ts
+++ src/resolvers/query/index.ts
@@ -7,7 +7,26 @@ const Query = {
     
     },
     async jokes(parent, args, { url, req }, info) {
-        return fetch(`${url}/random`)
+        return fetch(`${url}/jokes/random`)
+            .then(res => res.json())
+            .then(body => {
+                return body;
+            });
+    },
+    async categoryJokes(parent, args, { url, req }, info) {
+        const { category } = args;
+
+        return fetch(`${url}/jokes/random?category=${category}`)
+            .then(res => res.json())
+            .then(body => {
+                console.log('a', body)
+                return body;
+            });
+    },
+    async categories(parent, args, { url, req }, info) {
+        const { category } = args;
+        console.log('HERE')
+        return fetch(`${url}/jokes/categories`)
             .then(res => res.json())
             .then(body => {
                 return body;
